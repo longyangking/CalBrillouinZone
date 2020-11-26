3cb9a4486127de41d292a2bcbff05c7495c9afd1
Yang Long <yanglong@Mac-4.local>
2020-11-26 22:09:16 +0800

    update unitcell

--- lattice/symmetry.py
+++ lattice/symmetry.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+class Symmetry2D:
+    def __init__(self, group_number, verbose=False):
+        self.group_number = group_number
+        
+
+        self.verbose = verbose
--- lattice/unitcell.py
+++ lattice/unitcell.py
@@ -1,1 +1,38 @@
 import numpy as np
+
+class UnitCell2D:
+    def __init__(self, lattice_vectors, verbose=False):
+        self.lattice_vectors = lattice_vectors
+        self.verbose = verbose
+
+        self.reciprocal_lattice_vectors = self.__get_reciprocal_lattice_vectors()
+
+    def __get_reciprocal_lattice_vectors(self):
+        if self.verbose:
+            print("Starting to calculate the reciprocal lattice vectors...", end="")
+
+        # TODO 
+
+        if self.verbose:
+            print("Done!")
+
+    def get_high_symmetry_points(self):
+        
+
+
+class UnitCell3D:
+    def __init__(self, lattice_vectors, verbose=False):
+        self.lattice_vectors = lattice_vectors
+        self.verbose = verbose
+
+        self.reciprocal_lattice_vectors = self.__get_reciprocal_lattice_vectors()
+
+    def __get_reciprocal_lattice_vectors(self):
+        if self.verbose:
+            print("Starting to calculate the reciprocal lattice vectors...", end="")
+
+        # TODO 
+
+        if self.verbose:
+            print("Done!")
+
